3eb3dc9bc097c873edfe52b0c41afb8b70bc0576
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2021-12-17 19:24:39 -0700

    Wrote main menu prompt text

---
+++ ReadingListMaker/ReadingListMaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReadingListMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! This program searches for books and " +
+                "creates a reading list.");
+            Console.WriteLine();
+            Console.WriteLine("Please enter one of the following:");
+            Console.WriteLine();
+            Console.WriteLine("To look up a book by title, enter 1.");
+            Console.WriteLine("To view your current reading list, enter 2.");
+            Console.WriteLine("To quit, enter 3.");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
