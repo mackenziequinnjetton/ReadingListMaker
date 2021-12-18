b24cfe3d929940a1a66ac54c7a5cfa37d3ef3851
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2021-12-17 19:43:55 -0700

    Moved introductory text to its own function

--- ReadingListMaker/ReadingListMaker/Program.cs
+++ ReadingListMaker/ReadingListMaker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReadingListMaker
 {
@@ -9,18 +10,29 @@ namespace ReadingListMaker
             
         }
 
-        static string MainMenu()
+        static void IntroText()
         {
             Console.WriteLine("Welcome! This program searches for books and " +
                 "creates a reading list.");
             Console.WriteLine();
             Console.WriteLine("Please enter one of the following:");
             Console.WriteLine();
+        }
+
+        static string MainMenu()
+        {
             Console.WriteLine("To look up a book by title, enter 1.");
             Console.WriteLine("To view your current reading list, enter 2.");
             Console.WriteLine("To quit, enter 3.");
             Console.WriteLine();
-            Console.ReadLine();
+            
+            var response = Console.ReadLine();
+
+            if (new List<string> {"1", "2", "3"}.Contains(response.Trim()))
+            {
+                return response;
+            }
+            else { }
         }
     }
 }
