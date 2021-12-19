e661d3680cb4211083ab102e7d422acc46fcb55d
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2021-12-18 17:02:48 -0700

    Added BookCollection object

---
+++ ReadingListMaker/ReadingListMaker/BookCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingListMaker
+{
+    internal class BookCollection
+    {
+        public string kind { get; set; }
+        public string totalItems { get; set; }
+    }
+}
